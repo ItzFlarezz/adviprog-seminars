140dc7ea1332e88156e444ac0db4a44453ee80e1
sangn <sangn@kth.se>
2026-09-10 09:36:40 +0200

    small fixes

--- socketio-demo/app.js
+++ socketio-demo/app.js
@@ -17,34 +17,35 @@ app.get("/", (req, res) => {
 
 // Socket.IO connection handler
 // In app.js, modify the connection handler
-io.on('connection', (socket) => {
-  console.log('A user connected');
+io.on("connection", (socket) => {
+	console.log("A user connected");
 
-  // Store username with socket
-  socket.username = 'Anonymous';
-  // Handle new messages with username
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', {
-      username: socket.username,
-      message: msg,
-      timestamp: new Date().toISOString()
-    });
-  });
-  // Handle username change
-  socket.on('set username', (username) => {
-    const oldUsername = socket.username;
-    socket.username = username || 'Anonymous';
-    io.emit('user joined', {
-      oldUsername: oldUsername,
-      newUsername: socket.username
-    });
-  });
+	// Store username with socket
+	socket.username = "Anon";
+	// Handle new messages with username
+	socket.on("chat message", (msg) => {
+		io.emit("chat message", {
+			// This is instead of the loop sending the message to everyone.
+			username: socket.username,
+			message: msg,
+			timestamp: new Date().toISOString(),
+		});
+	});
+	// Handle username change
+	socket.on("set username", (username) => {
+		const oldUsername = socket.username;
+		socket.username = username || "Anon";
+		io.emit("user joined", {
+			oldUsername: oldUsername,
+			newUsername: socket.username,
+		});
+	});
 
-  // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-    io.emit('user left', { username: socket.username });
-  });
+	// Handle disconnection
+	socket.on("disconnect", () => {
+		console.log("A user disconnected");
+		io.emit("user left", { username: socket.username });
+	});
 });
 
 const PORT = process.env.PORT || 3000;
